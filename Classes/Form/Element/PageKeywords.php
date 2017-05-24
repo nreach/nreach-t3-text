4794a132a5e1d4bd7d8687f2cc0a2c187e0c9fed
ksjogo <jogo@kronberger-spiele.de>
2017-05-24 13:59:29 +0200

    Fix inheritance

--- Classes/Form/Element/PageKeywords.php
+++ Classes/Form/Element/PageKeywords.php
@@ -3,21 +3,21 @@ namespace Nreach\T3Text\Form\Element;
 
 use YoastSeoForTypo3\YoastSeo\Controller\ModuleController;
 
-class PageKeywords extends Base
+class PageKeywords extends \Nreach\T3Base\Form\Element\Base
 {
-   public function render()
-   {
-       $pageContentUrl = vsprintf(
-           '/index.php?id=%d&type=%d&L=%d',
-           [(int) $this->data['vanillaUid'],
-            ModuleController::FE_PREVIEW_TYPE,
-            0]);
+    public function render()
+    {
+        $pageContentUrl = vsprintf(
+            '/index.php?id=%d&type=%d&L=%d',
+            [(int) $this->data['vanillaUid'],
+             ModuleController::FE_PREVIEW_TYPE,
+             0]);
 
-       return array_merge_recursive(parent::render(), [
-           'linkAttributes' => [
-               'class' => 'nreacht3-pagekeywords ',
-               'data-url' => $pageContentUrl
-           ]
-       ]);
-   }
+        return array_merge_recursive(parent::render(), [
+            'linkAttributes' => [
+                'class' => 'nreacht3-pagekeywords nreach-btn-container ',
+                'data-url' => $pageContentUrl
+            ]
+        ]);
+    }
 }
--- ext_localconf.php
+++ ext_localconf.php
@@ -6,3 +6,6 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][] = [
     'class' => \Nreach\T3Text\Form\Element\PageKeywords::class,
     'priority' => 50,
 ];
+
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:nreach_t3_text/Configuration/RTE/Default.yaml';
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['t3kit_default'] = 'EXT:nreach_t3_text/Configuration/RTE/T3Kit.yaml';
